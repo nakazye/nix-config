46ac58549da45bd6645305c85fbfbb9b439698cd
nakazye <nakazye@gmail.com>
2025-02-10 10:58:04 +0000

    add font

--- nixos/wsl-configuration.nix
+++ nixos/wsl-configuration.nix
@@ -4,5 +4,11 @@
       experimental-features = ["nix-command" "flakes"];
     };
   };
+
+  fonts.packages = with pkgs; [
+    hackgen-nf-font
+    nerdfonts
+  ];
+  
   system.stateVersion = "24.05";
 }
