d20ad164e751b5886f88b03f87c882408e17d39d
nakazye <nakazye@gmail.com>
2025-02-10 10:20:22 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "My nix config";
+
+  inputs = {
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
+
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+      self,
+      nixpkgs,
+      nixos-wsl,
+      home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+    overlays = import ./overlays {inherit inputs;};
+    nixosModules = import ./modules/nixos;
+    homeManagerModules = import ./modules/home-manager;
+
+    # Available through 'sudo nixos-rebuild switch --flake .#wsl-nixos'
+    nixosConfigurations = {
+      wsl-nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          ./nixos/wsl-configuration.nix
+          nixos-wsl.nixosModules.default
+          {
+            system.stateVersion = "24.05";
+            wsl.enable = true;
+          }
+        ];
+      };
+    };
+
+    # Available through 'home-manager switch --flake .#nixos@wsl-nixos'
+    homeConfigurations = {
+      "nixos@wsl-nixos" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [
+          ./home-manager/wsl-home.nix
+        ];
+      };
+    };
+  };
+}
---
+++ nixos/wsl-configuration.nix
@@ -0,0 +1,8 @@
+{ config, lib, pkgs, ... }: {
+  nix = {
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+    };
+  };
+  system.stateVersion = "24.05";
+}
