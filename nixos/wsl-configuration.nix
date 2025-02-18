4cf953e1f8c6cac90696b48ecbf555557e79c86e
nakazye <nakazye@gmail.com>
2025-02-18 10:51:17 +0000

    remove IME

--- nixos/wsl-configuration.nix
+++ nixos/wsl-configuration.nix
@@ -5,17 +5,15 @@
     };
   };
 
+
   fonts.packages = with pkgs; [
     hackgen-nf-font
     nerdfonts
     
   ];
 
-  i18n.inputMethod = {
-    enable = true;
-    type = "ibus";
-    ibus.engines = with pkgs.ibus-engines; [ anthy ];
-  };
+  
+
   
   system.stateVersion = "24.11";
 }
