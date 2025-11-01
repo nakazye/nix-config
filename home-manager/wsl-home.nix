1451c1efc965e6a0a634ba9db2d9378acc177be1
nakazye <nakazye@gmail.com>
2025-11-01 07:04:39 +0000

    update wsl

--- home-manager/wsl-home.nix
+++ home-manager/wsl-home.nix
@@ -48,34 +48,131 @@
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
   home.packages = with pkgs; [
-    # Tools
+    cmake
+    glibtool
     unar
     chezmoi
+    tree
     fd
     ripgrep
-    tree-sitter
     ghq
-    peco
-    mozc
-    tree
-    # Lang
-    gnumake
-    cmake
-    gcc
-    libtool
-    jdk
-    python3
-    # App
-    (emacs.override {
-      withTreeSitter = true;
-      withNativeCompilation = true;
-      # Windowsとのクリップボード連携がおかしくなるのでいったんコメントアウト
-#      withXwidgets = true;
-#      withPgtk = true;
-    })
+    fzf
+    emacs
     vim
     neovim
   ];
+
+  programs.zsh = {
+    enable = true;
+
+    # auto_cd (ディレクトリ名だけでcd)
+    autocd = true;
+
+    # Emacsライクキーバインド
+    defaultKeymap = "emacs";
+
+    # シェルオプション
+    initExtra = ''
+      # ビープ音を鳴らさない
+      setopt nolistbeep
+
+      # もしかして機能
+      setopt correct
+
+      # 補完が詰めて表示される
+      setopt list_packed
+
+      # ディレクトリのスラッシュを削除しない
+      setopt noautoremoveslash
+
+      # 移動したディレクトリの記憶
+      setopt auto_pushd
+
+      # エイリアスの補完
+      setopt complete_aliases
+
+      # 色設定
+      autoload -U colors; colors
+
+      # プロンプト指定
+      PROMPT="
+      [%n] %{''${fg[yellow]}%}%~%{''${reset_color}%}
+      %(?.%{$fg[green]%}.%{$fg[blue]%})%(?!(*'-') <!(*;-;%)? <)%{''${reset_color}%} "
+      PROMPT2='[%n]> '
+
+      # VCS情報の設定
+      autoload -Uz vcs_info
+      setopt prompt_subst
+      zstyle ':vcs_info:git:*' check-for-changes true
+      zstyle ':vcs_info:git:*' stagedstr "%F{yellow}!"
+      zstyle ':vcs_info:git:*' unstagedstr "%F{red}+"
+      zstyle ':vcs_info:*' formats "%F{green}%c%u[%b]%f"
+      zstyle ':vcs_info:*' actionformats '[%b|%a]'
+      precmd () { vcs_info }
+      RPROMPT=$RPROMPT'$'{vcs_info_msg_0_}
+
+      # もしかして機能のプロンプト
+      SPROMPT="%{$fg[red]%}%{$suggest%}(*'~'%)? < %B%r%b %{$fg[red]%}? [Yes!(y), No!(n),a,e]:''${reset_color} "
+
+      # 履歴検索のキーバインド
+      autoload history-search-end
+      zle -N history-beginning-search-backward-end history-search-end
+      zle -N history-beginning-search-forward-end history-search-end
+      bindkey "^P" history-beginning-search-backward-end
+      bindkey "^N" history-beginning-search-forward-end
+    '';
+
+    # エイリアス
+    shellAliases = {
+      # Mac用
+      # ls = "ls -G";
+      # ll = "ls -lG";
+      # la = "ls -laG";
+      #Linux用
+      ls = "ls --color";
+      ll = "ls -l --color";
+      la = "ls -la --color";
+      du = "du -h";
+      df = "df -h";
+      vi = "nvim";
+      ec = "emacsclient -n";
+      rep = "cd $(ghq list -p | fzf -e)";
+    };
+
+    # コマンド履歴設定
+    history = {
+      size = 10000;
+      save = 10000;
+      path = "${config.home.homeDirectory}/.zsh_history";
+      ignoreDups = true;
+      share = true;
+    };
+
+    # 補完設定
+    enableCompletion = true;
+    completionInit = ''
+      autoload -U compinit
+      compinit
+
+      zstyle ':completion:*:default' menu select=2
+      zstyle ':completion:*' verbose yes
+      zstyle ':completion:*' completer _expand _complete _match _prefix _approximate _list _history
+      zstyle ':completion:*:messages' format '%F{YELLOW}%d'$DEFAULT
+      zstyle ':completion:*:warnings' format '%F{RED}No matches for: %F{YELLOW}%d'$DEFAULT
+      zstyle ':completion:*:descriptions' format '%F{YELLOW}completing %B%d%b'$DEFAULT
+      zstyle ':completion:*:options' description 'yes'
+      zstyle ':completion:*:descriptions' format '%F{yellow}Completing %B%d%b%f'$DEFAULT
+      zstyle ':completion:*' group-name ""
+      zstyle ':completion:*' list-separator '-->'
+      zstyle ':completion:*:manuals' separate-sections true
+    '';
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    nix-direnv.enable = true;
+  };
 
   programs.git = {
     extraConfig = {
@@ -85,14 +182,18 @@
         editor = "nvim";
         sshCommand = "/mnt/c/Windows/System32/OpenSSH/ssh.exe";
       };
-#      init = {
-#        defaultBranch = "main";
-#      };
+      init = {
+        defaultBranch = "master";
+      };
       ghq = {
         root = "~/src";
       };
     };
   };
+
+  programs.neovim.plugins = [
+    pkgs.vimPlugins.nvim-treesitter
+  ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
@@ -103,4 +204,5 @@
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "25.05";
+
 }
