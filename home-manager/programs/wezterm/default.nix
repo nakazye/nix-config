7857ed9bf7fb8c9924fa5906d0828a5542200c04
nakazye <nakazye@gmail.com>
2025-12-31 14:06:46 +0900

    weztermのタブを削除

--- home-manager/programs/wezterm/default.nix
+++ home-manager/programs/wezterm/default.nix
@@ -13,7 +13,7 @@
       config.font_size = 18.0
       config.use_ime = true
       config.macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL'
-      config.show_tabs_in_tab_bar = false
+      config.enable_tab_bar = false
       config.disable_default_key_bindings = true
 
       config.keys = {
