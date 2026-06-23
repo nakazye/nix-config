c8715a82e1f67c1d3bc3747512c5c4c318340d56
nakazye <nakazye@gmail.com>
2026-06-23 18:10:45 +0900

    home-manager: mermaid-cliにChrome指定のラッパーを追加

--- home-manager/programs/mermaid-cli/default.nix
+++ home-manager/programs/mermaid-cli/default.nix
@@ -1,3 +1,12 @@
-{pkgs, ...}: {
-  home.packages = [pkgs.mermaid-cli];
+{pkgs, ...}: let
+  chromePath =
+    if pkgs.stdenv.isDarwin
+    then "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+    else "${pkgs.chromium}/bin/chromium";
+  mmdc-wrapped = pkgs.writeShellScriptBin "mmdc" ''
+    export PUPPETEER_EXECUTABLE_PATH="${chromePath}"
+    exec ${pkgs.mermaid-cli}/bin/mmdc "$@"
+  '';
+in {
+  home.packages = [mmdc-wrapped];
 }
